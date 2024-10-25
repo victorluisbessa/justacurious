49f2e435741c782e33ce0989cee08bb7df2622e2
victorluisbessa <57152816+victorluisbessa@users.noreply.github.com>
2024-10-25 15:40:06 -0300

    Add files via upload

---
+++ shellms.ps1
@@ -0,0 +1,4 @@
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+
